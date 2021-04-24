ac3286030c80e67f0a3d3deeca148336de606c7c
Priyansh Sangule <psangule@schoology.com>
2021-04-24 22:23:28 +0530

    Add files

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void FixedUpdate(){
+      float moveSpeed = 7;
+      float horizontalInput = Input.GetAxis("Horizontal");
+      float verticalInput = Input.GetAxis("Vertical");
+      transform.Translate(new Vector3(horizontalInput, verticalInput, 0 ) * moveSpeed * Time.deltaTime);
+    }
+}
